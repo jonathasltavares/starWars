8b4b192e7be0a9f87b837d9f7ddd43a81c834bd3
Jonathas Tavares <jonathasl.tavares@gmail.com>
2024-02-23 12:48:19 -0300

    :construction: criando os componentes de home, filmes e nave

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmesComponent } from './pages/filmes/filmes.component';
+import { NaveComponent } from './pages/nave/nave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'filmes',
+    component: FilmesComponent,
+  },
+  {
+    path: 'nave',
+    component: NaveComponent,
+  },
+];
